ecfef7a54cd8ae940827a47ceccdf9c06301a3ad
Ginthozan Varnakulasingam <ginthozanv@gmail.com>
2025-08-15 23:51:14 +0200

    login validation

--- components/Pages/Auth/LoginForm.tsx
+++ components/Pages/Auth/LoginForm.tsx
@@ -1,22 +1,32 @@
 "use client";
 
-import { FormEvent } from "react";
+import { FormEvent, useRef, useState } from "react";
 import IconLockDots from "@/components/Icons/IconLockDots";
 import IconMail from "@/components/Icons/IconMail";
 import { signInCognitoUser } from "@/lib/aws-auth";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
+import { Field, Form, Formik, FormikProps } from "formik";
+import * as Yup from "yup";
 
 const LoginForm = () => {
   const router = useRouter();
-  
+  const [initialValues, setInitialValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [validationSchema, setValidationSchema] = useState(
+    Yup.object().shape({
+      email: Yup.string().email("Invalid email").required("Email is required"),
+      password: Yup.string().required("Password is required"),
+    })
+  );
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const formikRef = useRef<FormikProps<any>>(null);
 
-    const formData = new FormData(e.currentTarget);
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
+  const handleSubmit = async (values: any) => {
+    const email = values.email;
+    const password = values.password;
 
     const userLoggedInDetails = await signInCognitoUser(email, password);
     if (userLoggedInDetails.error) {
@@ -27,44 +37,69 @@ const LoginForm = () => {
   };
 
   return (
-    <form className="space-y-5" onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="Email">Email</label>
-        <div className="relative text-white-dark">
-          <input
-            id="Email"
-            name="email"
-            type="email"
-            placeholder="Enter Email"
-            className="form-input ps-10 placeholder:text-white-dark"
-          />
-          <span className="absolute start-4 top-1/2 -translate-y-1/2">
-            <IconMail fill={true} />
-          </span>
-        </div>
-      </div>
-      <div>
-        <label htmlFor="Password">Password</label>
-        <div className="relative text-white-dark">
-          <input
-            id="Password"
-            name="password"
-            type="password"
-            placeholder="Enter Password"
-            className="form-input ps-10 placeholder:text-white-dark"
-          />
-          <span className="absolute start-4 top-1/2 -translate-y-1/2">
-            <IconLockDots fill={true} />
-          </span>
-        </div>
-      </div>
-      <button
-        type="submit"
-        className="btn btn-gradient !mt-6 w-full border-0 uppercase shadow-[0_10px_20px_-10px_rgba(67,97,238,0.44)]"
-      >
-        Sign in
-      </button>
-    </form>
+    <Formik
+      ref={formikRef}
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      validateOnBlur={false}
+      validateOnChange={false}
+      onSubmit={(values: any, actions) => {
+        handleSubmit(values).then(() => {
+          actions.setSubmitting(false);
+          actions.resetForm();
+        });
+      }}
+    >
+      {({ handleSubmit, errors, touched, isSubmitting }: any) => {
+        return (
+          <Form onSubmit={handleSubmit} className="space-y-5">
+            <div>
+              <label htmlFor="Email">Email</label>
+              <div className="relative text-white-dark">
+                <Field
+                  id="Email"
+                  name="email"
+                  type="email"
+                  placeholder="Enter Email"
+                  className="form-input ps-10 placeholder:text-white-dark"
+                />
+                <span className="absolute start-4 top-1/2 -translate-y-1/2">
+                  <IconMail fill={true} />
+                </span>
+              </div>
+              {errors.email && touched.email && (
+                <small className="text-red-500">{errors.email}</small>
+              )}
+            </div>
+            <div>
+              <label htmlFor="Password">Password</label>
+              <div className="relative text-white-dark">
+                <Field
+                  id="Password"
+                  name="password"
+                  type="password"
+                  placeholder="Enter Password"
+                  className="form-input ps-10 placeholder:text-white-dark"
+                />
+                <span className="absolute start-4 top-1/2 -translate-y-1/2">
+                  <IconLockDots fill={true} />
+                </span>
+              </div>
+              {errors.password && touched.password && (
+                <small className="text-red-500">{errors.password}</small>
+              )}
+            </div>
+            <button
+              type="submit"
+              className="btn btn-gradient !mt-6 w-full border-0 uppercase shadow-[0_10px_20px_-10px_rgba(67,97,238,0.44)]"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "Signing in..." : "Sign in"}
+            </button>
+          </Form>
+        );
+      }}
+    </Formik>
   );
 };
 
