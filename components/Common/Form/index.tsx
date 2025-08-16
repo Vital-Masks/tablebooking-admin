606ca561116cb3eb2fc1be3a6f29d51bdc71214b
Ginthozan Varnakulasingam <ginthozanv@gmail.com>
2025-08-16 17:16:01 +0200

    hospitality chain password change logiv

--- components/Common/Form/index.tsx
+++ components/Common/Form/index.tsx
@@ -1,5 +1,5 @@
 import { Field, Form, Formik, FormikProps } from "formik";
-import { IconXCircle } from "@/components/Icons";
+import { IconXCircle, IconEye, IconEyeOff } from "@/components/Icons";
 import IconLoading from "@/components/Icons/IconLoading";
 import CustomDatesCalendar from "../Fields/CustomDatesCalendar";
 import RestaurantSelect from "@/components/Elements/RestaurantSelect";
@@ -8,7 +8,7 @@ import DiningAreaSelect from "@/components/Elements/DiningAreaSelect";
 import Calendar from "../Fields/Calendar";
 import SelectField from "../Fields/SelectField";
 import FilePicker from "../Fields/FilePicker";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const options = [{ value: "select", label: "Select" }];
 
@@ -159,6 +159,53 @@ const FormComponent = ({
   );
 };
 
+interface PasswordFieldProps {
+  field: FormField;
+  errors: any;
+}
+
+const PasswordField: React.FC<PasswordFieldProps> = ({ field, errors }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div key={field.id} className={`${errors[field.name] && "has-error"}`}>
+      <label
+        htmlFor={field.name}
+        className={field.disabled ? "text-gray-500" : ""}
+      >
+        {field.label}
+      </label>
+      <div className="relative">
+        <Field
+          id={field.name}
+          name={field.name}
+          type={showPassword ? "text" : "password"}
+          placeholder={field.placeholder}
+          className={`form-input pr-10 ${field.disabled ? "bg-gray-100 text-gray-500 cursor-not-allowed opacity-60" : ""}`}
+          disabled={field.disabled}
+        />
+        <button
+          type="button"
+          onClick={togglePasswordVisibility}
+          className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none"
+          disabled={field.disabled}
+        >
+          {showPassword ? (
+            <IconEyeOff className="w-4 h-4" />
+          ) : (
+            <IconEye className="w-4 h-4" />
+          )}
+        </button>
+      </div>
+      <div className="text-danger mt-1 text-xs">{errors[field.name]}</div>
+    </div>
+  );
+};
+
 interface RenderFieldProps {
   field: FormField;
   errors: any;
@@ -180,7 +227,12 @@ const RenderField: React.FC<RenderFieldProps> = ({
             key={field.id}
             className={`${errors[field.name] && "has-error"}`}
           >
-            <label htmlFor={field.name} className={field.disabled ? "text-gray-500" : ""}>{field.label}</label>
+            <label
+              htmlFor={field.name}
+              className={field.disabled ? "text-gray-500" : ""}
+            >
+              {field.label}
+            </label>
             <Field
               name={field.name}
               component={SelectField}
@@ -199,30 +251,20 @@ const RenderField: React.FC<RenderFieldProps> = ({
               key={field.id}
               className={`${errors[field.name] && "has-error"}`}
             >
-              <label htmlFor={field.name} className={field.disabled ? "text-gray-500" : ""}>{field.label}</label>
-
-              {/* <Field name={field.name} as="select" className="form-input">
-                <option key="0">Select</option>
-                {field.options
-                  ? field.options?.map((option) => (
-                      <option key={option.value} value={option.value}>
-                        {option.label}
-                      </option>
-                    ))
-                  : options.map((option) => (
-                      <option key={option.value} value={option.value}>
-                        {option.label}
-                      </option>
-                    ))}
-              </Field> */}
+              <label
+                htmlFor={field.name}
+                className={field.disabled ? "text-gray-500" : ""}
+              >
+                {field.label}
+              </label>
               <Field
-              name={field.name}
-              component={SelectField}
-              options={field.options}
-              isMulti={field.isMulti}
-              hasError={errors[field.name]}
-              disabled={field.disabled}
-            />
+                name={field.name}
+                component={SelectField}
+                options={field.options}
+                isMulti={field.isMulti}
+                hasError={errors[field.name]}
+                disabled={field.disabled}
+              />
 
               <div className="text-danger mt-1 text-xs">
                 {errors[field.name]}
@@ -261,8 +303,15 @@ const RenderField: React.FC<RenderFieldProps> = ({
     case "switch":
       return (
         <div key={field.id}>
-          <label htmlFor={field.name} className={field.disabled ? "text-gray-500" : ""}>{field.label}</label>
-          <label className={`relative h-6 w-12 ${field.disabled ? "opacity-60 cursor-not-allowed" : ""}`}>
+          <label
+            htmlFor={field.name}
+            className={field.disabled ? "text-gray-500" : ""}
+          >
+            {field.label}
+          </label>
+          <label
+            className={`relative h-6 w-12 ${field.disabled ? "opacity-60 cursor-not-allowed" : ""}`}
+          >
             <Field
               type="checkbox"
               className="custom_switch peer absolute z-10 h-full w-full cursor-pointer opacity-0"
@@ -278,7 +327,12 @@ const RenderField: React.FC<RenderFieldProps> = ({
     case "textarea":
       return (
         <div key={field.id} className={`${errors[field.name] && "has-error"}`}>
-          <label htmlFor={field.name} className={field.disabled ? "text-gray-500" : ""}>{field.label}</label>
+          <label
+            htmlFor={field.name}
+            className={field.disabled ? "text-gray-500" : ""}
+          >
+            {field.label}
+          </label>
           <Field
             id={field.name}
             name={field.name}
@@ -330,6 +384,8 @@ const RenderField: React.FC<RenderFieldProps> = ({
           setFieldValue={setFieldValue}
         />
       );
+    case "password":
+      return <PasswordField field={field} errors={errors} />;
     default:
       // Check if field should be conditionally rendered
       if (field.ifRender) {
@@ -339,7 +395,12 @@ const RenderField: React.FC<RenderFieldProps> = ({
               key={field.id}
               className={`${errors[field.name] && "has-error"}`}
             >
-              <label htmlFor={field.name} className={field.disabled ? "text-gray-500" : ""}>{field.label}</label>
+              <label
+                htmlFor={field.name}
+                className={field.disabled ? "text-gray-500" : ""}
+              >
+                {field.label}
+              </label>
               <Field
                 id={field.name}
                 name={field.name}
@@ -358,9 +419,18 @@ const RenderField: React.FC<RenderFieldProps> = ({
       }
 
       // Default rendering for fields without conditional logic
+      if (field.type === "password") {
+        return <PasswordField field={field} errors={errors} />;
+      }
+
       return (
         <div key={field.id} className={`${errors[field.name] && "has-error"}`}>
-          <label htmlFor={field.name} className={field.disabled ? "text-gray-500" : ""}>{field.label}</label>
+          <label
+            htmlFor={field.name}
+            className={field.disabled ? "text-gray-500" : ""}
+          >
+            {field.label}
+          </label>
           <Field
             id={field.name}
             name={field.name}
