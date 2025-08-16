04caca645024e97aae0b1061a4f137c5bdc782ca
Ginthozan Varnakulasingam <ginthozanv@gmail.com>
2025-08-16 17:20:16 +0200

    login page reveal password

--- components/Pages/Auth/LoginForm.tsx
+++ components/Pages/Auth/LoginForm.tsx
@@ -3,6 +3,7 @@
 import { FormEvent, useRef, useState } from "react";
 import IconLockDots from "@/components/Icons/IconLockDots";
 import IconMail from "@/components/Icons/IconMail";
+import { IconEye, IconEyeOff } from "@/components/Icons";
 import { signInCognitoUser } from "@/lib/aws-auth";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
@@ -11,6 +12,7 @@ import * as Yup from "yup";
 
 const LoginForm = () => {
   const router = useRouter();
+  const [showPassword, setShowPassword] = useState(false);
   const [initialValues, setInitialValues] = useState({
     email: "",
     password: "",
@@ -83,13 +85,24 @@ const LoginForm = () => {
                 <Field
                   id="Password"
                   name="password"
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   placeholder="Enter Password"
-                  className="form-input ps-10 placeholder:text-white-dark"
+                  className="form-input ps-10 pe-10 placeholder:text-white-dark"
                 />
                 <span className="absolute start-4 top-1/2 -translate-y-1/2">
                   <IconLockDots fill={true} />
                 </span>
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute end-4 top-1/2 -translate-y-1/2 text-white-dark hover:text-white-dark/80 focus:outline-none"
+                >
+                  {showPassword ? (
+                    <IconEyeOff className="w-4 h-4" />
+                  ) : (
+                    <IconEye className="w-4 h-4" />
+                  )}
+                </button>
               </div>
               {errors.password && touched.password && (
                 <small className="text-red-500">{errors.password}</small>
